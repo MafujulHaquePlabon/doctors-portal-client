cf33c551fff7736fe8c15341cdddf21b508ce2b1
MafujulHaquePlabon <mafujul15-3076@diu.edu.bd>
2022-05-16 17:17:35 +0600

    contact us,added appointment banner,lift up state and available appointments,added services card,display booking modal on booking button click,display treatment info in the modal,load data from database

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,7 +3,15 @@ import React from 'react';
 const Login = () => {
     return (
         <div>
-            <h1>This is Login page</h1>
+           <div class="card w-96 bg-base-100 shadow-xl">
+  <div class="card-body">
+    <h2 class="card-title">Card title!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
         </div>
     );
 };
