f20ad135f602570d15785db143869637b0eeb390
MafujulHaquePlabon <mafujul15-3076@diu.edu.bd>
2022-05-16 16:56:47 +0600

    banner,info card,services section,home page make appointment section, testimonials section footer added

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Navbar from './Pages/Shared/Navbar';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Login from './Pages/Login/Login';
+import Appointment from './Pages/Appointment/Appointment';
+
+function App() {
+  return (
+    <div className='max-w-7xl mx-auto px-12'>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="appointment" element={<Appointment />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
