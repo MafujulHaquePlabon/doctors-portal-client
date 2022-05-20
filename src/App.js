7ba70baec9e039be8176fe7988c3ed56a7dd1002
MafujulHaquePlabon <mafujul15-3076@diu.edu.bd>
2022-05-20 18:49:54 +0600

    some code updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from './Pages/Shared/Navbar';
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
 import Login from './Pages/Login/Login';
